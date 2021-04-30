575db05f885a16dd49fef8c0c4b73b05cd205f78
Joan Pedro <jpos.senai@gmail.com>
2021-04-30 11:12:08 -0300

    feat: Obtaining data for child route by parent resolver

--- APM-Start/src/app/products/product-edit/product-edit-info.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit-info.component.ts
@@ -1,8 +1,7 @@
+import { Product } from './../product';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Data } from '@angular/router';
 import { NgForm } from '@angular/forms';
-
-import { Product } from '../product';
 
 @Component({
   templateUrl: './product-edit-info.component.html'
@@ -11,10 +10,15 @@ export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm, { static: true }) productForm: NgForm;
 
   errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test', description: 'test' };
+  product: Product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.parent.data.subscribe(
+      (data: Data) => {
+        this.product = data['resolvedData'].product;
+      }
+    )
   }
 }
